7d6d6519edf8bdc714e0e70853feb0041f62e587
Finn Buick <finnbuick@gmail.com>
2020-11-24 22:07:19 +1100

    updated readme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,38 @@
 import React from "react"
+import { Helmet } from "react-helmet"
+import Layout from "../components/layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Finn Buick</title>
+        <link rel="canonical" href="http://finnbuick.com/" />
+      </Helmet>
+
+      <section className="s1">
+        <div className="main-container">
+          <h1>Hey, I'm Finn Buick.</h1>
+          <p>
+            I'm a budding engineer based in Sydney who likes to build things and
+            learn stuffs, pls hire
+          </p>
+          <a href="#">Link</a>
+        </div>
+      </section>
+
+      <section className="s2">
+        <div className="main-container">About</div>
+      </section>
+
+      <section className="s2">
+        <div className="main-container">Portfolio</div>
+      </section>
+
+      <section className="s1">
+        <div className="main-container">Contact</div>
+      </section>
+    </Layout>
+  )
 }
