3bd732dda7ade4255becf0808938aed07186a90a
Finn Buick <finnbuick@gmail.com>
2020-11-25 17:47:10 +1100

    updated layout and added content

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,24 +13,70 @@ export default function Home() {
 
       <section className="s1">
         <div className="main-container">
-          <h1>Hey, I'm Finn Buick.</h1>
-          <p>
-            I'm a budding engineer based in Sydney who likes to build things and
-            learn stuffs, pls hire
-          </p>
-          <a href="#">Link</a>
+          <div className="greeting-wrapper">
+            {/* <h4>Hey, my name is</h4> */}
+            <h1>Hi, I'm Finn Buick</h1>
+            <h4>
+              I'm a software engineer, based in Sydney. I like to learn, solve
+              problems and build useful things.
+            </h4>
+            <p>
+              Get in touch{" "}
+              <span className="emoji technologist" role="img">
+                👉
+              </span>{" "}
+              <a href="mailto:finnbuick@gmail.com">finnbuick@gmail.com</a>
+            </p>
+          </div>
         </div>
       </section>
 
       <section className="s2">
-        <div className="main-container">About</div>
-      </section>
+        <div className="about-wrapper">
+          <div className="about-me">
+            <div className="main-container">
+              <h3>About Me</h3>
+              <p>
+                I'm a fourth year software engineering student at Macquarie
+                University. I like to build things for the web.
+              </p>
+              <a href="Resume.pdf" target="_blank">
+                Download Resume
+              </a>
+              <p>Here are a few technologies I've have experience with:</p>
+              <div id="skills">
+                <ul>
+                  <li>React</li>
+                  <li>Javascript</li>
+                  <li>Typescript</li>
+                  <li>HTML & CSS</li>
+                </ul>
+                <ul>
+                  <li>Python</li>
+                  <li>Java</li>
+                  <li>SQL</li>
+                </ul>
+              </div>
+            </div>
+          </div>
 
-      <section className="s2">
-        <div className="main-container">Portfolio</div>
+          <div className="social-links">
+            {/* <img id="social_img" src="images/headshot.jpg" alt="" /> */}
+            <a href="#" target="_blank">
+              LinkedIn
+            </a>
+            <a href="#" target="_blank">
+              Github
+            </a>
+          </div>
+        </div>
       </section>
 
       <section className="s1">
+        <div className="main-container">Portfolio</div>
+      </section>
+
+      <section className="s2">
         <div className="main-container">Contact</div>
       </section>
     </Layout>
