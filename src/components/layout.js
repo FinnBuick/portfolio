8a6f3b1be22dbfba37c0937d6ba9ed811ae9e379
Finn Buick <finnbuick@gmail.com>
2020-11-26 15:21:24 +1100

    added breakpoints for mobile

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import React from "react"
-import "fontsource-inconsolata/800.css"
+import "fontsource-inconsolata/700.css"
 
 export default function Layout({ children }) {
   return <div>{children}</div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,77 +7,82 @@ export default function Home() {
     <Layout>
       <Helmet>
         <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
         <title>Finn Buick</title>
         <link rel="canonical" href="http://finnbuick.com/" />
       </Helmet>
 
-      <section className="s1">
-        <div className="main-container">
-          <div className="greeting-wrapper">
-            {/* <h4>Hey, my name is</h4> */}
-            <h1>Hi, I'm Finn Buick</h1>
-            <h4>
-              I'm a software engineer, based in Sydney. I like to learn, solve
-              problems and build useful things.
-            </h4>
-            <p>
-              Get in touch{" "}
-              <span className="emoji technologist" role="img">
-                👉
-              </span>{" "}
-              <a href="mailto:finnbuick@gmail.com">finnbuick@gmail.com</a>
-            </p>
+      <header className="intro">
+        {/* <h4>Hey, my name is</h4> */}
+        <h1 className="intro__hello">Hi, I'm Finn Buick</h1>
+        <h2 className="intro__tagline">
+          I'm a software engineer, based in Sydney. <br />I like to learn, solve
+          problems and build useful things.
+        </h2>
+        <h3 className="intro__contact">
+          Get in touch{" "}
+          <span className="sm-view" role="img">
+            👇
+          </span>
+          <span className="lg-view" role="img">
+            👉
+          </span>{" "}
+          <a href="mailto:finnbuick@gmail.com">finnbuick@gmail.com</a>
+        </h3>
+      </header>
+
+      <section className="section background">
+        <div className="section__title">Background</div>
+        <div className="section__content">
+          <p>
+            I'm a fourth year software engineering student at Macquarie
+            University. I like to build things for the web.
+          </p>
+        </div>
+        <p>Here are a few technologies I have experience with:</p>
+        <div className="skills">
+          <div className="skills__category">
+            <ul>
+              <li>Javascript</li>
+              <li>Typescript</li>
+              <li>HTML & CSS</li>
+              <li>Python</li>
+              <li>Java</li>
+              <li>SQL</li>
+            </ul>
+          </div>
+          <div className="skills__category">
+            <ul>
+              <li>React</li>
+              <li>Material-UI</li>
+              <li>Firebase</li>
+              <li>Express</li>
+            </ul>
           </div>
         </div>
       </section>
 
-      <section className="s2">
-        <div className="about-wrapper">
-          <div className="about-me">
-            <div className="main-container">
-              <h3>About Me</h3>
-              <p>
-                I'm a fourth year software engineering student at Macquarie
-                University. I like to build things for the web.
-              </p>
-              <a href="Resume.pdf" target="_blank">
-                Download Resume
-              </a>
-              <p>Here are a few technologies I've have experience with:</p>
-              <div id="skills">
-                <ul>
-                  <li>React</li>
-                  <li>Javascript</li>
-                  <li>Typescript</li>
-                  <li>HTML & CSS</li>
-                </ul>
-                <ul>
-                  <li>Python</li>
-                  <li>Java</li>
-                  <li>SQL</li>
-                </ul>
-              </div>
-            </div>
-          </div>
+      <section className="section projects">
+        <div>Projects</div>
+      </section>
 
-          <div className="social-links">
-            {/* <img id="social_img" src="images/headshot.jpg" alt="" /> */}
-            <a href="#" target="_blank">
-              LinkedIn
-            </a>
-            <a href="#" target="_blank">
-              Github
-            </a>
-          </div>
+      <section className="section contact">
+        <div>Contact</div>
+        <div className="socialLinks">
+          <img id="social_img" src="images/headshot.jpg" alt="" />
+          <a href="#" target="_blank">
+            LinkedIn
+          </a>
+          <a href="#" target="_blank">
+            Github
+          </a>
+          <a href="Resume.pdf" target="_blank">
+            Download Resume
+          </a>
         </div>
-      </section>
-
-      <section className="s1">
-        <div className="main-container">Portfolio</div>
-      </section>
-
-      <section className="s2">
-        <div className="main-container">Contact</div>
       </section>
     </Layout>
   )
