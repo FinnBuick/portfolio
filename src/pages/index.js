8db04747fcf3dd30c2d981c87db46eadb554246b
FinnBuick <finnbuick@gmail.com>
2020-12-28 21:44:06 +1100

    added analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,16 @@ export default function Home() {
         ></meta>
         <title>Finn Buick</title>
         <link rel="canonical" href="http://finnbuick.com/" />
+        
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-508D1LDCFM"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-508D1LDCFM');
+        </script>
       </Helmet>
       <Intro />
       <Background />
